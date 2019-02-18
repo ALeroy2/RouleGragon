0777105a2837f1ac9205b80af6205a098a92b241
aloishias <hias.alois@gmail.com>
2019-02-18 20:28:08 +0100

    Ajout de quelques paramètres sur la classe actor, ainsi que du constructeur de item.

--- JS/Actor.js
+++ JS/Actor.js
@@ -1,7 +1,10 @@
 class Actor {
   constructor (options){
     this.name = options.name;
-    this.image = options.image
+    this.image = options.image;
+    this.life = options.life;
+    this.attack = options.attacks;
+    this.shield = options.shield
   }
   fight(){
 
