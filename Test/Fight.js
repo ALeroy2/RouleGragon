3a01bc007ad6160cdfc99077a1413dfb764effbd
tguyot <guyot.thomas97@gmail.com>
2019-02-18 16:45:07 +0100

    pierre img

--- Test/Fight.js
+++ Test/Fight.js
@@ -1,3 +1,8 @@
 import Monster from "../JS/Monster.js";
-var mon = new Monster({name:"Pierre", image:"Test"});
+var mon = new Monster({name:"Pierre", image:"https://cdn.discordapp.com/attachments/547037631322783757/547080433175887872/664573925_small.png"});
 alert(mon.name);
+
+$('#ok').text(mon.name);
+var img = $('<img id="dynamic_img">');
+img.attr('src', mon.image);
+img.appendTo('#ok');
