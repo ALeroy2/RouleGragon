5bbfe5194a41d1e064ac7e5b950225c6fce0d880
tguyot <guyot.thomas97@gmail.com>
2019-02-18 16:48:47 +0100

    ajout monstre visu

--- Test/Fight.js
+++ Test/Fight.js
@@ -1,8 +1,8 @@
 import Monster from "../JS/Monster.js";
 var mon = new Monster({name:"Pierre", image:"https://cdn.discordapp.com/attachments/547037631322783757/547080433175887872/664573925_small.png"});
 alert(mon.name);
-
-$('#ok').text(mon.name);
-var img = $('<img id="dynamic_img">');
-img.attr('src', mon.image);
-img.appendTo('#ok');
+var nom_mob = $('<p>'+mon.name+'</p>');
+nom_mob.appendTo('#ok');
+var img_mob = $('<img id="dynamic_img">');
+img_mob.attr('src', mon.image);
+img_mob.appendTo('#ok');
