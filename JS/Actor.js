56787f6cb1c79ecb14bf8c4cf0bc60b8aebc9e3c
tguyot <guyot.thomas97@gmail.com>
2019-02-19 13:03:51 +0100

    commit actor modif

--- JS/Actor.js
+++ JS/Actor.js
@@ -1,10 +1,12 @@
 class Actor {
   constructor (options){
     this.name = options.name;
-    this.image = options.image
+    this.image = options.image;
+    this.life = options.life;
+    this.attack = options.attack;
   }
-  fight(){
-
+  fight(options){
+    options.target.life = this.attack
   }
 }
 
